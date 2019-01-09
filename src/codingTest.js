c77a9fdcb06cc8ff56990f6641102655ef2a4b9c
thekiiingbob <robertlubecker@gmail.com>
2019-01-09 06:43:22 -0600

    Porting over test to JS

---
+++ src/codingTest.js
@@ -0,0 +1,13 @@
+// Feel free to use, ignore or modify this code as you see fit
+
+function loadFile(name) {}
+
+function dayMinTempSpread(fileName) {
+  throw new Error("dayMinTempSpread not implemented!");
+}
+
+function teamMinPointSpread(fileName) {
+  throw new Error("teamMinPointSpread not implemented!");
+}
+
+module.exports = { loadFile, dayMinTempSpread, teamMinPointSpread };
